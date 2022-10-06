1b7a377c412d2d7aa3c5894a75932a4be8d48c72
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-10-06 15:37:15 -0700

    create basic file stracture for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import './App.css'
-import { Router, Route, Routes} from 'react-router-dom'
-import {Box, NavBar} from '@mui/material';
+import { Route, Routes} from 'react-router-dom'
+import {Box} from '@mui/material';
+import Home from './pages/Home'
+import Navbar from './components/Navbar';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Box width='400px'> 
-      <NavBar />
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/exercise/:id' element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   )
 }
