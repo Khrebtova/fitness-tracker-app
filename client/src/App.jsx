c11ad9c7f8441938cb65b51abb6daaccbfc8f5cf
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-10-06 15:29:23 -0700

    install material UI and update package.json dependecies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './App.css'
+import { Router, Route, Routes} from 'react-router-dom'
+import {Box, NavBar} from '@mui/material';
 
 const App = () => {
   return (
-    <div> your App</div>
+    <Box width='400px'> 
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
   )
 }
 
